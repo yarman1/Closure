93d030a1eb692fb58ae35179972fa2a7b291aca7
yarman1 <gordienko045@gmail.com>
2021-11-06 19:56:39 +0200

    lab5

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const array = [];
+  const fn = (i) => array[i];
+  fn.push = (element) => array.push(element);
+  fn.pop = () => array.pop();
+  return fn;
+};
 
 module.exports = { array };
